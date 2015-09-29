4dbf087fe7fbd3a105d75a075def4ecb62bd94ab
Brent Daniel <brent23961@hotmail.com>
2015-09-29 08:25:34 -0600

    Increased k to 10^7 and added timing capability.

--- Pi.java
+++ Pi.java
@@ -2,12 +2,15 @@
 class Pi {
 
 	public static void main(String[] args) {
-		System.out.println( "pi: " + pi() );
+		long start = System.currentTimeMillis();
+		double result = pi();
+		long end = System.currentTimeMillis();
+		System.out.println( "pi: " + result + ", time: " + (end-start) );
 	}
 
 	public static double pi() {
 		double sum=0.0;
-		for(double k=0.0; k<50.0; k++) {
+		for(double k=0.0; k<10000000.0; k++) {
 			sum += Math.pow(-3.0, -k) / (2.0*k+1.0);
 		}
 		return Math.sqrt(12.0)*sum;
